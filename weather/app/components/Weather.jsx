f2d2a53b41e45dea42b98a8402302b9e38db81a4
Brian Mont <briansmont830@gmail.com>
2016-12-20 17:08:53 +0000

    weather fake call

--- weather/app/components/Weather.jsx
+++ weather/app/components/Weather.jsx
@@ -3,11 +3,23 @@ var WeatherForm = require('WeatherForm');
 var WeatherMessage = require('WeatherMessage');
 
 var Weather = React.createClass({
+  getInitialState: function() {
+    return {
+      location: 'Miami',
+      temp: 88,
+    }
+  },
+  handleSearch: function(location) {
+    this.setState({
+      location: location,
+      temp: 23,
+    });
+  },
   render: function() {
     return(
       <div>
         <h3>Weather Component</h3>
-        <WeatherForm/>
+        <WeatherForm onSearch={this.handleSearch}/>
         <WeatherMessage/>
       </div>
     );
--- weather/public/bundle.js
+++ weather/public/bundle.js
@@ -24934,6 +24934,18 @@
 	var Weather = React.createClass({
 	  displayName: 'Weather',
 
+	  getInitialState: function getInitialState() {
+	    return {
+	      location: 'Miami',
+	      temp: 88
+	    };
+	  },
+	  handleSearch: function handleSearch(location) {
+	    this.setState({
+	      location: location,
+	      temp: 23
+	    });
+	  },
 	  render: function render() {
 	    return React.createElement(
 	      'div',
@@ -24943,7 +24955,7 @@
 	        null,
 	        'Weather Component'
 	      ),
-	      React.createElement(WeatherForm, null),
+	      React.createElement(WeatherForm, { onSearch: this.handleSearch }),
 	      React.createElement(WeatherMessage, null)
 	    );
 	  }
@@ -24955,25 +24967,34 @@
 /* 219 */
 /***/ function(module, exports, __webpack_require__) {
 
-	"use strict";
+	'use strict';
 
 	var React = __webpack_require__(1);
 
 	var WeatherForm = React.createClass({
-	  displayName: "WeatherForm",
+	  displayName: 'WeatherForm',
 
+	  onFormSubmit: function onFormSubmit(e) {
+	    e.preventDefault();
+
+	    var location = this.refs.location.value;
+	    if (location.length > 0) {
+	      this.refs.location.value = '';
+	      this.props.onSearch(location);
+	    }
+	  },
 	  render: function render() {
 	    return React.createElement(
-	      "div",
+	      'div',
 	      null,
 	      React.createElement(
-	        "form",
-	        null,
-	        React.createElement("input", { type: "text" }),
+	        'form',
+	        { onSubmit: this.onFormSubmit },
+	        React.createElement('input', { type: 'text', ref: 'location' }),
 	        React.createElement(
-	          "button",
+	          'button',
 	          null,
-	          "Get Weather"
+	          'See Weather'
 	        )
 	      )
 	    );
