f35d7783526e9b03f03e7653b037c25e3cd899bc
Brian Mont <briansmont830@gmail.com>
2016-12-22 15:15:17 +0000

    updated about.jsx

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -2,7 +2,10 @@ var React = require('react');
 
 var About = (props) => {
   return(
-      <h3>About Component</h3>
+      <div>
+        <h3>About</h3>
+        <p>This is a basic weather application that fetches weather from whatever city you submit</p>
+      </div>
     );
 };
 
