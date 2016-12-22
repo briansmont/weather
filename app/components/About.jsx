ccd717c6c0a7f1283e42019815e8f0072c6fb859
Brian Mont <briansmont830@gmail.com>
2016-12-22 22:10:53 +0000

    updated about page in the application, style & copy with resources linked

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -3,8 +3,23 @@ var React = require('react');
 var About = (props) => {
   return(
       <div>
-        <h3>About</h3>
-        <p>This is a basic weather application that fetches weather from whatever city you submit</p>
+        <h1 className="text-center">About</h1>
+        <p>Basic Weather Application - You request we provide!</p>
+        
+        <p>Here are some of the tools and info used in this app:</p>
+        <ul>
+          <li>
+            <a href="https://facebook.github.io/react" target="_blank">React</a> - This was the JS library used.
+          </li>
+          <li>
+            <a href="http://openweathermap.org/" target="_blank">Open Weather Map</a> - Their API provides all of our weather data! You search the city name, and they provide the information.
+          </li>
+          <li>
+            <a href="https://github.com/briansmont/learn-react" target="_blank">Weather App Repo</a> - My github repo.
+          </li>
+
+          
+        </ul>
       </div>
     );
 };
