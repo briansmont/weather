64d705b94185cb999c919b778bf71d51420cf789
Brian Mont <briansmont830@gmail.com>
2016-12-22 20:20:36 +0000

    style main component, centered using Foundation, also added examples to select to examples.jsx

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,9 +1,22 @@
 var React = require('react');
+var {Link} = require('react-router');
+
 
 var Examples = (props) => {
   return(
-    <h3>Examples</h3>
-  )
+    <div>
+      <h1 className="text-center">Examples</h1>
+      <p>Here are a few examples to try out</p>
+      <ol>
+        <li>
+          <Link to='/?location=Philadelphia'>Philadelphia, PA</Link>
+        </li>
+        <li>
+          <Link to='/?location=Winterfell'>Winterfell, Westeros</Link>
+        </li>
+      </ol>
+    </div>
+  );
 };
 
 module.exports = Examples;
