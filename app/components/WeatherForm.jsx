06e6380445b6c21457e34de64566ac45bae017dc
Brian Mont <briansmont830@gmail.com>
2016-12-23 18:21:35 +0000

    modal works now after a user submits an invalid query, ALSO autofocus on weatherForm.jsx search input

--- app/components/WeatherForm.jsx
+++ app/components/WeatherForm.jsx
@@ -9,13 +9,13 @@ var WeatherForm = React.createClass({
       this.refs.location.value = '';
       this.props.onSearch(location);
     }
-    
   },
+  
   render: function() {
     return (
       <div>
         <form onSubmit={this.onFormSubmit}>
-          <input type="search" ref="location" placeholder="Search weather by city"/>
+          <input autoFocus type="search" ref="location" placeholder="Search weather by city"/>
           <button className="button expanded hollow">Get Weather</button>
         </form>
       </div>
